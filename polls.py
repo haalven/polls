854831f1a1964e3a620e7b6b909279447c9cabe2
haalven <147269352+haalven@users.noreply.github.com>
2026-02-12 12:01:46 +0100

    Enhance poll data structure and regression settings
    Added state column to poll data and adjusted filtering criteria. Updated LOWESS regression fraction based on selected options.

--- polls.py
+++ polls.py
@@ -8,6 +8,7 @@ import argparse, sys, os.path, tomllib
 import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+from datetime import datetime as dt
 
 # xterm formatting
 def f(code): return '\x1B[' + str(code) + 'm'
@@ -40,6 +41,10 @@ def get_arguments(my_name):
                         action='store_true',
                         required=False,
                         help='use all pollsters')
+    parser.add_argument('-s', '--show',
+                        action='store_true',
+                        required=False,
+                        help='show data points')
     return parser.parse_args()
 
 
@@ -61,11 +66,17 @@ def main() -> int:
     except Exception as e:
         warn('error reading csv data from: ' + config['csvurl'] + ' failed: ' + str(e))
         return 'error reading csv data'
+    # save csv file
+    today = dt.now().date().isoformat()
+    data_file = os.path.splitext(my_name)[0] + '-original-' + today + '.csv'
+    df.to_csv(os.path.join(my_dir, data_file), index=False)
 
     # select columns
-    polldata = df[['end_date', 'poll_id', 'pollster', 'politician', 'yes', 'no']]
+    polldata = df[['end_date', 'poll_id', 'pollster', 'state', 'politician', 'yes', 'no']]
     polldata = polldata.copy()
 
+    # filter for US polls
+    polldata = polldata[polldata['state'] == 'US']
     # filter for politician
     polldata = polldata[polldata['politician'] == 'Donald Trump']
 
@@ -76,6 +87,9 @@ def main() -> int:
         polldata = polldata[polldata['pollster'].isin(config['selected_pollsters'])]
     else:
         print(' all pollsters (bad quality)')
+    # source link
+    print(f(1) + 'source:' + f(0))
+    print(config['nyturl'])
 
     # remove duplicates
     poll_ids = []
@@ -98,21 +112,35 @@ def main() -> int:
     print(polldata[['dt_date', 'pollster', 'margin']].tail(15))
 
     # LOWESS regression
-    lowess = sm.nonparametric.lowess(polldata['margin'], polldata['dt_date'], frac=.5)
+    frac = .35 if (config['selected_only'] and not arguments.all) else .3
+    lowess = sm.nonparametric.lowess(polldata['margin'], polldata['dt_date'], frac=frac)
     trend = lowess[:, 1]
     print(f(1) + 'latest regression:' + f(0))
     print(trend[-1])
+    # save regression
+    today = dt.now().date().isoformat()
+    data_file = os.path.splitext(my_name)[0] + '-regression-' + today + '.csv'
+    regr_df = pd.DataFrame({'Date': polldata['dt_date'], 'Pollster': polldata['pollster'], 'Margin': polldata['margin'], 'Regression': trend, 'Diff': polldata['margin']-trend})
+    # round and reverse
+    regr_df = regr_df.round(1).iloc[::-1]
+    regr_df.to_csv(os.path.join(my_dir, data_file), index=False)
+
+    # show data points
+    if arguments.show:
+        os.system(f'vd {os.path.join(my_dir, data_file)}')
+        return 0
 
     # setup matplotlib
     fig, ax = plt.subplots(figsize=(6, 6))
     # title & footnote
     plt.title('Trump 2nd Term Polls: Margin (%)')
-    fig.text(0.01, 0.01, 'source: ' + config['nyturl'], fontsize=7, color='gray')
+    #fig.text(0.01, 0.01, 'source: ' + config['nyturl'], fontsize=7, color='gray')
+    fig.text(0.01, 0.01, 'CNN/SSRS, Gallup, Ipsos, NYT/Siena, Pew, Quinnipac', fontsize=7, color='gray')
 
     # y-limits and horizontal lines
-    ax.set(ylim=(-30, 20))
+    ax.set(ylim=(-30, 10))
     plt.axhline(0, color='black', linewidth=1)
-    for y in (20, 10, -10, -20):
+    for y in (-10, -20, -30):
         plt.axhline(y, color='lightgrey', linewidth=1)
 
     # margin scatter
